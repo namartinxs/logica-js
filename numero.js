f5053cdcbdb3d617656e958f9caef215d46a365e
Nathalia Martins <nathaliafloresmartins4550@gmail.com>
2024-06-23 19:30:55 -0300

    positivo,negativo ou zero

---
+++ numero.js
@@ -0,0 +1,9 @@
+let numero = prompt ('Insira um número');
+
+if (numero < 0){
+    console.log ('Este número é negativo');
+}else if (numero == 0){
+    console.log('Este numero é igual a zero');
+}else{
+    console.log ('Este número é positivo');
+}
