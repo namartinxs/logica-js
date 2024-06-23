b81af7afa03a8ff2ab0a50c8fc1b8f5506749a30
Nathalia Martins <nathaliafloresmartins4550@gmail.com>
2024-06-23 13:44:08 -0300

    Crie um programa de contagem progressiva

---
+++ contadorprogressivo.js
@@ -0,0 +1,7 @@
+let resposta = prompt("Insira um numero para a contagem progressiva");
+let contador = 0;
+
+while (contador <=resposta){
+    console.log(`Contando de zero a ${resposta}.Numero atual ${contador}`)
+    contador++
+}
