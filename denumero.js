70fc9a00057f5e7c1f6ff0bca79f246e86eff292
Nathalia Martins <nathaliafloresmartins4550@gmail.com>
2024-06-23 01:25:16 -0300

    Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.

---
+++ denumero.js
@@ -0,0 +1,9 @@
+alert ('Bem vindo ao verificador de números');
+let numero = prompt('Este número é positivo ou negativo?');
+
+if (numero >0){
+    alert('Este número é positivo.');
+}
+else{
+    alert('Este número é negativo.');
+}
