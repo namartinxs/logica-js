605391e6407cefe2422935da28deb5b8761fc75a
Nathalia Martins <nathaliafloresmartins4550@gmail.com>
2024-06-23 01:24:36 -0300

    Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!

---
+++ desemana.js
@@ -0,0 +1,9 @@
+let dia = prompt('Em que dia da semana você está?');
+let saudacaofim = "Bom fim de semana!";
+let saudacaosemana = "Boa semana!";
+if (dia == 'sabado' || dia == 'domingo'){
+    alert(saudacaofim);
+}
+else{
+    alert(saudacaosemana);
+}
