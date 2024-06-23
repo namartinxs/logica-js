50b00a5a24a7a2cca758c4ba2d62f58f3d1d3b58
Nathalia Martins <nathaliafloresmartins4550@gmail.com>
2024-06-23 13:43:50 -0300

    Crie um programa de contagem regressiva.

---
+++ contadorregressivo.js
@@ -0,0 +1,8 @@
+let resposta = prompt("Insira um número para a contagem regressiva");
+let contador = resposta
+
+while(contador>=0){
+    console.log(`Contando de ${resposta} até zero. Numero atual ${contador}`);
+    contador = contador -1;
+
+}
