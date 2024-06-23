4797f52e202d06596151a6e6c03735fe3152cf0a
Nathalia Martins <nathaliafloresmartins4550@gmail.com>
2024-06-23 13:14:22 -0300

    loop while

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 alert('Bem-Vindo ao jogo do número secreto');
 let numeroSecreto = 29;
-console.log(numeroSecreto)
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-let chute = prompt('Escolha um número entre 1 e 30');
-console.log('Valor da tentativa', chute);
-
-console.log('Comparação entre tentativa e número secreto' ,chute==numeroSecreto)
-if (numeroSecreto == chute){
+//enquanto numeroSecreto nao for igual ao chute
+while (numeroSecreto  != chute){
+    chute = prompt('Escolha um número entre 1 e 30');
+    //numero secreto e chute são iguais
+    if (numeroSecreto == chute){
     //template strings são feitas com crase
-   alert(`Parabéns você acertou o número secreto ${numeroSecreto}`)
+        alert(`Parabéns você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+    } else{
+        if(numeroSecreto > chute){
+            alert(`O número secreto é maior que ${chute}`);
+        } else{
+            alert(`O número secreto é menor que ${chute}`);
+        }
+
+        tentativas++;
+    }
 }
-else{
-    alert('Você errou :( O número secreto era'  + numeroSecreto +  'Tente outra vez!')
-}
+
 
